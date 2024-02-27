cf1b5dcd03ee84f28ecf95969fc074307e30edcb
Marwan <yassinimarwan6@gmail.com>
2024-02-27 16:52:09 +0000

    jijifd

--- disease-mapper/src/App.js
+++ disease-mapper/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p>disease mapper</p>
+       <p>Disease Mapper</p>
       </header>
     </div>
   );
