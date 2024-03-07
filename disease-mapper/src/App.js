a216c85e4dc92ae5c7c41cd1bdf75becd4b59117
Marwan <yassinimarwan6@gmail.com>
2024-03-07 15:43:43 +0000

    hope

--- disease-mapper/src/App.js
+++ disease-mapper/src/App.js
@@ -1,41 +1,17 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import { Search } from 'react-bootstrap-icons';
 import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
-
-    const handleFetch = () => {
-        // This is the flask server url
-        fetch("http://localhost:" + process.env.FLASK_PORT + "/search", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "test1": 1,
-                "test2": 2,
-            })
-        }).then((res) => res.json()
-            .then((result) => {
-                console.log(result.status);
-                console.log(result.message);
-            })
-        );
-    }
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="primary">Primary</Button>{' '}
+        <Button variant="secondary">Secondary</Button>{' '}
+        <Button variant="success">Success</Button>{' '}
+      </header>
+    </div>
+  );
 }
 
 export default App;
