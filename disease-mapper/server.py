1e10c1bcaf1f49c9e588d982520071f8470d1ab0
Raghav <raghavawasthi2005@gmail.com>
2024-03-07 16:27:12 +0000

    WE CREATED A TABLE AND CONNECTED DB

---
+++ disease-mapper/server.py
@@ -0,0 +1,59 @@
+import os
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import mysql.connector
+from flask_cors import CORS, cross_origin
+
+load_dotenv()
+app = Flask(__name__)
+
+# MySQL setup
+# app.config["MYSQL_HOST"] = "localhost"
+# app.config["MYSQL_USER"] = "root"
+# app.config["MYSQL_PASSWORD"] = "root"
+# app.config["MYSQL_DB"] = "disease_mapper_db"
+# print(app.config)
+config = {
+    "user": "root",
+    "password": "root",
+    "host": "localhost",
+    "unix_socket": "/Applications/MAMP/tmp/mysql/mysql.sock",
+    "database": "disease_mapper_db",
+    "raise_on_warnings": True,
+}
+
+
+# Remove in production. This is for dev (same origin server) only.
+app.config["CORS_HEADERS"] = "Content-Type"
+cors = CORS(app)
+
+cnx = mysql.connector.connect(**config)
+
+@app.route("/search", methods=["POST"])
+@cross_origin() # Remove in production
+def search():
+    print('helloooo')
+    # data = request.get_json()
+    # test1 = data["test1"]
+    # test2 = data["test2"]
+    # cursor = cnx.cursor(dictionary=True)
+    # cursor.execute(
+    #     """ CREATE TABLE Users (
+    #         userID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    #         username varchar(255) NOT NULL,
+    #         password varchar(255) NOT NULL,
+    #         NHSID varchar(255) NOT NULL,
+    #         postcode varchar(255) NOT NULL
+    #     )"""
+    # )
+    # # cursor.execute(""" INSERT INTO QUERY VALUES(%s,%s)""", (test1, test2))
+    # cnx.close()
+    return {"status": "SUCCESS", "message": "test message"}
+
+
+@app.route('/', defaults = {"path": ""})
+def test(path):
+    return "Hello World"
+
+if __name__ == "__main__":
+    app.run(debug = True, host = "localhost", port = os.getenv("FLASK_PORT"))
--- disease-mapper/src/App.js
+++ disease-mapper/src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Button from 'react-bootstrap/Button';
 import './App.css';
+// import { useEffect } from 'react';
+// import 'dotenv/config';
+const search = async () => {
+    const response = await fetch("http://localhost:3001/search", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: "HI"
+    });
+    return response;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant="primary">Primary</Button>{' '}
-        <Button variant="secondary">Secondary</Button>{' '}
-        <Button variant="success">Success</Button>{' '}
+        <Button onClick={search} variant="primary">Primary</Button>{' '}
+        {/* <Button variant="secondary">Secondary</Button>{' '}
+        <Button variant="success">Success</Button>{' '} */}
       </header>
     </div>
   );
