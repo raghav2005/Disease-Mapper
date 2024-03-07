b6ab83d2cc2f14e321b3f41f60b89c222d4136e9
Raghav <raghavawasthi2005@gmail.com>
2024-03-07 19:01:59 +0000

    first try collab

--- disease-mapper/src/App.js
+++ disease-mapper/src/App.js
@@ -1,15 +1,19 @@
 import Button from 'react-bootstrap/Button';
 import React from 'react';
 import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+
 import './App.css';
 import 'leaflet/dist/leaflet.css'; // Make sure to import the Leaflet CSS
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const search = async () => {
     const response = await fetch("http://localhost:" + process.env.REACT_APP_FLASK_PORT + "/search", {
         method: "POST",
         headers: { "content-type": "application/json" },
-        body: "HI"
+        body: "hello"
     });
     return response;
 }
@@ -17,16 +21,26 @@ const search = async () => {
 function App() {
   // Ensure everything you want to render is inside the return statement
     return (
-        <div className="App">
-
-            <header className="App-header">
-                <Button onClick={search} variant="primary">Primary</Button>{' '}
-                {/* <Button variant="secondary">Secondary</Button>{' '}
-                <Button variant="success">Success</Button>{' '} */}
-            </header>
-
-            {/* The map container must have a defined height, ensure this is set in your CSS */}
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "800px" }}>
+        
+        <div className='App'>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Disease Mapper</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Report</Nav.Link>
+                    </Nav>
+                    <Nav className="me-auto" id='right_side_navbar'>
+                        <Nav.Link href="#features">Register</Nav.Link>
+                        <Nav.Link href="#features">Login</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <br />
+       
+            
+              {/* The map container must have a defined height, ensure this is set in your CSS */}
+              <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "400px" }}>
                 <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -37,8 +51,8 @@ function App() {
                     </Popup>
                 </Marker>
             </MapContainer>
-
         </div>
+          
     );
 }
 
--- disease-mapper/src/index.js
+++ disease-mapper/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
+import no from './App';
 import reportWebVitals from './reportWebVitals';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<no />}>
+                    {/* <Route index element={} />
+                    <Route path="blogs" element={<Blogs />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="*" element={<NoPage />} /> */}
+                </Route>
+              </Routes>
+        </BrowserRouter>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
