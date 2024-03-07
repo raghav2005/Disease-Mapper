a7745660d7f5cb66b6826393f459f669e245dbcf
Cindy Yun <cindy@Cindys-MBP.localdomain>
2024-03-07 16:05:01 +0000

    map

--- disease-mapper/src/App.js
+++ disease-mapper/src/App.js
@@ -1,23 +1,25 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'leaflet/dist/leaflet.css'; // Make sure to import the Leaflet CSS
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function App() {
+  // Ensure everything you want to render is inside the return statement
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* The map container must have a defined height, ensure this is set in your CSS */}
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "800px" }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
